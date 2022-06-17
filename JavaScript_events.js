a34034a837d40a4342d468f667243eedaaabe4af
Mohamed Aziz Belkahla <mohamedazizbelkahla@gmail.com>
2022-06-17 09:10:49 +0100

    a

--- JavaScript_events.js
+++ JavaScript_events.js
@@ -1,7 +1,21 @@
 // Grab elements
-
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error(`Something went, make sure that &{selector} exists or is typed correctly.`);
+};
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated');
+    }else{
+        headerElement.classList.remove('activated');
 
+    }    
+
+    
+};
 // Open menu & search pop-up
 
 // Open/Close search form popup
@@ -10,4 +24,4 @@
 
 // Switch theme/add to local storage
 
-// Swiper
+// Swiper 
